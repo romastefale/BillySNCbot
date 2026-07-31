529703ad0100f4bdb14fdc80be12b658eefb2a2b
romastefale <romastefale@gmail.com>
2026-07-31 16:53:33 -0300

    Activate reaction block before bot dispatcher imports

--- myjamrobot/app/bot/__init__.py
+++ myjamrobot/app/bot/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from app.bot.reactionless_bot import install_reaction_block
+
+install_reaction_block()
+
 from app.bot.telegram import bot_dispatcher
 
 __all__ = ["bot_dispatcher"]
